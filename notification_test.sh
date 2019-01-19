2bc399b2abbcd62603a64ddef0ea9510424ccb45
Chris Meyers <cm.02.93@gmail.com>
2019-01-18 22:57:04 -0700

    added notification_test.sh script.

---
+++ notification_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "usage: $0 [-h] [-r]\n"
+      echo ""
+      echo "optional arguments:"
+      echo " -h, --help    shows this help message and exit"
+      echo " -r, --reload  reloads the dunst program"
+      exit 0
+      ;;
+    -r|--reload)
+      killall dunst
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+notify-send "Low" "Something low happended on `date`!" --urgency=low
+notify-send "Normal" "Something normal happended on `date`!" --urgency=normal
+notify-send "Critical" "Something critical happended on `date`!" --urgency=critical
