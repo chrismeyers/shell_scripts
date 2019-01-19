f7bb4e74f7abf5d3ffd9884183e2b6a3e9239bcb
Chris Meyers <cm.02.93@gmail.com>
2019-01-18 22:54:34 -0700

    updated location of backlight brightness.

--- backlight_percent.sh
+++ backlight_percent.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-current=`cat /sys/class/backlight/intel_backlight/brightness`
+current=`cat /sys/class/backlight/intel_backlight/actual_brightness`
 max=`cat /sys/class/backlight/intel_backlight/max_brightness`
 
 percent=`echo "($current / $max) * 100" | bc -l`
